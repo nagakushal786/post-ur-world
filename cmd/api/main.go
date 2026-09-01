38d6e485bc94bfdfbbb41fc4331164e103ad3f71
GitHub Action <action@github.com>
2026-09-01 15:07:16 +0000

    Update version to 1.2.0

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -19,7 +19,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const version = "1.1.0"
+const version = "1.2.0"
 
 // @title Post Ur World API
 // @description API for post ur world, a platform to interact with fellow engineers
